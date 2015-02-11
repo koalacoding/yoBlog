44b25a9d696f48436f5e7786ac237bd480bdedfd
koalacoding <koalacoding@net-c.fr>
2015-02-11 21:34:02 +0100

    Added view_post to see a single blog post.

---
+++ view_blog/view_post/view_post.php
@@ -0,0 +1,83 @@
+<?php include('../../to_include.php'); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../view_blog.css">
+		<meta charset="utf-8" />
+		<title>Blog</title>
+	</head>
+	<body>
+		<?php
+			if (isset($_GET['id'])) {
+				include ('../../sql_connexion.php');
+
+				$request = $bdd->prepare("SELECT * FROM posts WHERE id=?");
+				$request->execute(array($_GET['id']));
+
+				if ($request->rowCount() > 0) {
+					$data = $request->fetch();
+		?>
+					<div id="core">
+						<div id="header">
+							<div id="banner">
+							Blog of <?php echo $data['author']?>
+							</div>
+						</div>
+						<div id="menu">
+							<div>
+								<span class="menu_element_left actual_page">Blog</span>
+								<a href="" class="menu_element_left">About</a>
+								<a href="" class="menu_element_left">Contact</a>
+							</div>
+							<div class="menu_element_right">
+								<a href="../../index.php">Return to index</a>
+							</div>
+						</div>
+
+						<div id="right_core">
+							<div class="right_core_group">
+								<span class="right_core_title">Last comments</span>
+								<p>John : GOod blog</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+								<p>Mike : About 33 ?</p>
+							</div>
+
+							<div class="right_core_group">
+								<span class="right_core_title">Archives</span>
+							</div>
+						</div>
+
+
+						<div id="left_core">
+							<div class="entry">
+								<span class="title">Title : <?php echo htmlspecialchars($data['title']); ?></span>
+								<span class="post_date"><br />Published : <?php echo $data['post_date']; ?></span>
+								<span class="post"><?php echo htmlspecialchars($data['post']); ?><br /></span>
+							</div>
+
+		<?php
+				}
+
+				else {
+					echo "This post doesn't exist. Redirection in 2 seconds...";
+					header("refresh:2;url=index.php");
+				}
+			}
+
+			else {
+				echo "No post id specified. Redirection in 2 seconds...";
+				header("refresh:2;url=index.php");
+			}
+		?>
+	</body>
+</html>
