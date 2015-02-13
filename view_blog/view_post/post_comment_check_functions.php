36384a3d18045323184397114422c9cce175e439
koalacoding <koalacoding@net-c.fr>
2015-02-13 22:18:53 +0100

    Adding missing file.

---
+++ view_blog/view_post/post_comment_check_functions.php
@@ -0,0 +1,19 @@
+<?php
+
+function is_username_empty($username) {
+	if (!preg_match("#^.+$#", $username)) {
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+function is_email_valid($email) {
+	if (!preg_match("#^[a-zA-Z0-9-_]{1,17}@[a-zA-Z0-9-_]{1,17}.[a-zA-Z-]{1,7}$#", $email)) {
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+?>
